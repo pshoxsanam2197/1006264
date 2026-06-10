328f18f4d637242d3cac5f0647e4ef055e57695b
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-06-10 16:33:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# 3-m
+yosh = int(input("yosh kirit: "))
+
+if 0 <= yosh <= 12:
+    print("Bola")
+elif 13 <= yosh <= 17:
+    print("O'smir")
+elif 18 <= yosh <= 64:
+    print("Kattalar")
+else:
+    print("Keksa")
